c0be55af406359c3e5f7f0ed260c4fe23b8bf776
alV0o <voroshilov.olexandro07@mail.ru>
2026-02-07 00:26:37 +0500

    sp12

---
+++ client/client.cpp
@@ -0,0 +1,80 @@
+﻿// client.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+
+int main()
+{
+	setlocale(0, "rus");
+	while (true) {
+		system("cls");
+
+		HANDLE hMutex;
+
+		hMutex = OpenMutex(SYNCHRONIZE, FALSE, L"printer_mutex");
+		if (hMutex == NULL) {
+			system("cls");
+			std::cout << "Принтер выключен" << std::endl;
+			_getch();
+		}
+		else {
+			std::cout << "Нажмите чтобы начать печать либо поменять приоритет (a, b, c)..." << std::endl;
+
+			switch (_getch()) {
+				case 'a': {
+					SetPriorityClass(GetCurrentProcess(), IDLE_PRIORITY_CLASS);
+					std::cout << "Задан фоновый приоритет" << std::endl;
+					Sleep(1000);
+				}
+						break;
+				case 'b': {
+					SetPriorityClass(GetCurrentProcess(), NORMAL_PRIORITY_CLASS);
+					std::cout << "Задан нормальный приоритет" << std::endl;
+					Sleep(1000);
+				}
+						break;
+				case 'c': {
+					SetPriorityClass(GetCurrentProcess(), HIGH_PRIORITY_CLASS);
+					std::cout << "Задан высокий приоритет" << std::endl;
+					Sleep(1000);
+				}
+						break;
+				default: {
+
+					if (WaitForSingleObject(hMutex, 0) == WAIT_TIMEOUT) {
+						system("cls");
+						std::cout << "Мьютекс занят" << std::endl;
+						Sleep(1000);
+					}
+					else {
+						system("cls");
+						std::cout << "Задание отправлено на печать" << std::endl;
+						Sleep(1000);
+						ReleaseMutex(hMutex);
+						Sleep(1000);
+						WaitForSingleObject(hMutex, INFINITE);
+						system("cls");
+						std::cout << "Печать завершена" << std::endl;
+						ReleaseMutex(hMutex);
+
+						CloseHandle(hMutex);
+						Sleep(1000);
+					}
+				}
+			}
+		}
+	}
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ sp12_voroshilov/sp12_voroshilov.cpp
@@ -0,0 +1,67 @@
+﻿// sp12_voroshilov.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <Windows.h>
+#include <random>
+#include <conio.h>
+
+DWORD WINAPI Printer() {
+	std::cout << "Печать..." << std::endl;
+
+	std::random_device rd;   // non-deterministic generator
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dist(5, 15);
+
+	int sleeptime = dist(gen) * 1000;
+	Sleep(sleeptime);
+	return 0;
+}
+
+int main()
+{
+	setlocale(0, "rus");
+
+	HANDLE hMutex;
+	hMutex = CreateMutex(NULL, FALSE, L"printer_mutex");
+	if (hMutex == NULL) {
+		return GetLastError();
+	}
+	int timeLife = 0;
+	while (timeLife <= 600) {
+		std::cout << "Ждет..." << std::endl;
+
+		if (WaitForSingleObject(hMutex, 0) == WAIT_TIMEOUT) {
+			timeLife = 0;
+			system("cls");
+
+			HANDLE hThread;
+			DWORD IDThread;
+
+			WaitForSingleObject(hMutex, INFINITE);
+
+			hThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Printer, NULL, NULL, &IDThread);
+			if (hThread == NULL)
+				return GetLastError();
+
+			if (WaitForSingleObject(hThread, 10000) == WAIT_OBJECT_0) {
+				system("cls");
+				std::cout << "Готово" << std::endl;
+			}
+			else {
+				system("cls");
+				std::cout << "Ошибка печати" << std::endl;
+			}
+			CloseHandle(hThread);
+
+		}
+		else {
+			timeLife++;
+		}
+		ReleaseMutex(hMutex);
+		Sleep(1000);
+		system("cls");
+	}
+	std::cout << "Принтер отключился" << std::endl;
+	_getch();
+}
